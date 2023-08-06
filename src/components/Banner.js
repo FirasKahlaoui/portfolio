56967ce345f52d84ad9a418639dbf331335cdcbe
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2023-08-06 16:45:05 +0100

    Banner img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-use-before-define */
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/cartography-1801579.svg";
+import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
