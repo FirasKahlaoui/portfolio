89c58b01e04c7962a16f5ee43e29b35ddd024930
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-05-31 21:47:15 +0100

    add firebase settings

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,17 @@ import contactImg from "../assets/img/contact-img.svg";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
 export const Contact = () => {
   const formInitialDetails = {
     firstName: "",
